fe0821a95420d53dc88c18f6e09cd7b5da5fb315
Tom Flídr <tomflidr@gmail.com>
2018-12-13 18:20:13 +0100

    Doc comments and bugfixes

--- src/MvcCore/Ext/Routers/MediaAndLocalization/UrlByRouteSections.php
+++ src/MvcCore/Ext/Routers/MediaAndLocalization/UrlByRouteSections.php
@@ -66,7 +66,7 @@ trait UrlByRouteSections
 
 		// complete by given route base url address part and part with path and query string
 		list($urlBaseSection, $urlPathWithQuerySection) = $route->Url(
-			$this->request, $params, $defaultParams, $this->getQueryStringParamsSepatator()
+			$this->request, $params, $defaultParams, $this->getQueryStringParamsSepatator(), TRUE
 		);
 		
 
