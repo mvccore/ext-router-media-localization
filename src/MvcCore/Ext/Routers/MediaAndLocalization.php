1c2156af73749634ef91c596bc39cd7c515b9d6a
Tom Flídr <tomflidr@gmail.com>
2021-04-30 00:30:11 +0200

    release 5.0.2

--- src/MvcCore/Ext/Routers/MediaAndLocalization.php
+++ src/MvcCore/Ext/Routers/MediaAndLocalization.php
@@ -63,6 +63,6 @@ implements	\MvcCore\Ext\Routers\IMedia,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.1';
+	const VERSION = '5.0.2';
 
 }
